9e6eb60ec91db70b70168e412702e12c77df481f
Deepak Repo <dpk.ewit@gmail.com>
2026-02-18 23:49:55 +0530

    Add welcome message for trigger automation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Deepak welcome to java programming!");
+        System.out.println("trigger, welcome to trigger automatic done!");
     }
 }
  
